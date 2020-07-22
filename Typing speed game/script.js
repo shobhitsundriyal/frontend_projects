c044bf4bf16c9ee75bf1d1792bb388c49b7c3c9f
unknown <shobhitsundriyal0170@gmail.com>
2020-07-22 10:21:35 +0530

    to add event listener

--- Typing speed game/script.js
+++ Typing speed game/script.js
@@ -1,12 +1,39 @@
 const word = document.getElementById('word')
 const text = document.getElementById('text')
-const score = document.getElementById('score')
-const time  = document.getElementById('time')
-const endgame = document.getElementById('end-game')
+const scoreEl = document.getElementById('score')
+const timeEl  = document.getElementById('time')
+const endgameEl = document.getElementById('end-game')
 const settingsbtn = document.getElementById('settings-btn')
 const settings = document.getElementById('settings')
 const settingsForm = document.getElementById('settings-form')
 const difficultySelect = document.getElementById('difficulty')
 
-//comment
+//some words
+const words = [
+    'sign',
+    'tense',
+    'airplane',
+    'boat',
+    'javascript'
+]
 
+let randomWord;
+
+let score = 0;
+
+let time = 10;
+
+function getRandomWord() {
+    // Use api here later
+    return words[Math.floor(Math.random() * words.length)]
+}
+
+console.log(getRandomWord())
+
+function addWordToDOM() {
+    randomWord = getRandomWord()
+    word.innerHTML = randomWord
+}
+addWordToDOM()
+
+//Event Listener
