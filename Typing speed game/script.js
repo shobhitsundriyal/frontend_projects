0d2c92370b48f44c085a5e2a4a6c19074335772c
unknown <shobhitsundriyal0170@gmail.com>
2020-07-24 01:11:19 +0530

    add timer

--- Typing speed game/script.js
+++ Typing speed game/script.js
@@ -34,6 +34,21 @@ function addWordToDOM() {
     randomWord = getRandomWord()
     word.innerHTML = randomWord
 }
+
+function updateScore() {
+    score++
+    scoreEl.innerHTML = score;
+}
+
 addWordToDOM()
 
 //Event Listener
+text.addEventListener('input', e=>{
+    const insertedText = e.target.value; //captures whatever we type in text box
+    //console.log(insertedText)
+    if (insertedText == randomWord) {
+        addWordToDOM() //get new word
+        updateScore();
+        e.target.value = '' //clear the input text 
+    }
+})
