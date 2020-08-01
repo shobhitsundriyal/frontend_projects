c381a2416b3420f7df86f006594eb9a673acb940
unknown <shobhitsundriyal0170@gmail.com>
2020-08-01 20:45:19 +0530

    dark mode setup

--- Breakout Game/script.js
+++ Breakout Game/script.js
@@ -1,7 +1,19 @@
 const rulesbtn = document.getElementById('rules-btn')
 const closebtn = document.getElementById('close-btn')
 const rules = document.getElementById('rules')
+const dark = document.getElementById('dark-mode')
+const light = document.getElementById('light-mode')
+const body = document.getElementsByTagName("BODY")[0];
 
 // toggle rule
 rulesbtn.addEventListener('click', () => rules.classList.add('show'))
 closebtn.addEventListener('click', () => rules.classList.remove('show'))
+dark.addEventListener('click', () => {
+    light.classList.add('disp')
+    body.classList.add('dm')
+    // more things to add
+})
+light.addEventListener('click', () => {
+    body.classList.remove('dm')
+    light.classList.remove('disp')
+})
